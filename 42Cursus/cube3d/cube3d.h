4ffcfe80a134bd28c6d7f6c43cd8f60a9fe7d25b
Davide maria Forte <dforte@e3r1p7.42roma.it>
2022-09-13 15:53:32 +0200

    asd

--- 42Cursus/cube3d/cube3d.h
+++ 42Cursus/cube3d/cube3d.h
@@ -6,7 +6,7 @@
 /*   By: dforte <dforte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 17:20:26 by dforte            #+#    #+#             */
-/*   Updated: 2022/06/25 11:47:21 by dforte           ###   ########.fr       */
+/*   Updated: 2022/07/29 16:57:36 by dforte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,13 @@
 # define CUBE3D_H
 
 # define PI				3.14159265359
-# define P2				PI/2
-# define P3				3*P2
-# define DR				0.0174533
+# define RAD			0.0174533
 # define FOV			60
 # define SCREEN_WIDTH	640
 # define SCREEN_HEIGHT	480
 
-# define RED 16711680
-# define YELLOW 65025
-# define BLUE 255
-# define BLACK 0
-# define WHITE 16777215
-# define GREEN 65280
+# define SPEED	0.1
+# define SENS	0.06598132
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -35,12 +29,23 @@
 # include <stdio.h>
 # include "libft/libft.h"
 
+# define W		13
+# define A		0
+# define S		1
+# define D		2
+# define LEFT	123
+# define RIGHT	124
+# define ESC	53
+
 typedef struct s_img
 {
-	void	*p;
-	void	*w;
-	void	*f;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		ll;
+	int		e;
 }				t_img;
+
 
 typedef struct s_player
 {
@@ -48,40 +53,65 @@ typedef struct s_player
 	float	y;
 	float	dx;
 	float	dy;
-	float	a;
+	float	ldx;
+	float	ldy;
+	float	angle;
+	float	langle;
+	int		w;
+	int		s;
+	int		a;
+	int		d;
+	int		left;
+	int		right;
 }				t_player;
 
 typedef	struct s_cub3D
 {
-	int			height;
-	int			width;
 	char		*NO;
 	char		*SO;
 	char		*WE;
 	char		*EA;
-	char		*F;
-	char		*C;
+	int			F;
+	int			C;
 	char		**map;
+	int			height;
+	int			width;
+	int			s_h;
+	int			s_w;
 	void		*mlx;
 	void		*win;
-	float		dist[FOV];
-	float		wheight[FOV];
+	void		*gun1;
+	void		*gun2;
+	void		*gun;
+	float		dist[FOV * 16];
+	int			color[FOV * 16];
+	float		wheight[FOV * 16];
 	t_player	p;
 	t_img		img;
 }				t_cub3D;
 
+int		ft_on(int keycode, t_cub3D *data);
+int		ft_off(int keycode, t_cub3D *data);
 int		ft_get_height(char *file_name, char *str);
-int		ft_hooks(int keycode, t_cub3D *data);
 int		ft_3d_render(t_cub3D *data);
 char	*ft_get_row(char *str, int flag);
+void	ft_remove_newline(char *file_name);
+void	ft_check_file(char *file_name);
+void	ft_put_term(char *str);
+void	my_mlx_pixel_put(t_cub3D *data, int x, int y, int color);
+void	ft_check_angle(float *angle);
 void	get_wall_height(t_cub3D *data);
-void	ft_get_info(int fd, char *str, t_cub3D *data);
-void	ft_read_file(char *file_name, t_cub3D *data);
-void	ft_init(t_cub3D *data);
-void	find_player(t_cub3D *data, int *x, int *y);
-void	ft_free_struct(t_cub3D *data);
 void	get_distance(t_cub3D *data, float ray);
-void	ft_free_matrix(void	**matrix);
-void	 ft_get_width(t_cub3D *data);
+void	ft_read_file(char *file_name, t_cub3D *data);
+void	ft_get_width(t_cub3D *data);
+void	ft_get_info(int fd, char *str, t_cub3D *data);
+void	ft_move(t_cub3D	*data, float move_x, float move_y);
+void	ft_movement(t_cub3D *data);
+void	ft_dir(t_cub3D *data, float dir);
+void	ft_init(t_cub3D *data);
+void	ft_init_img(void);
+void	ft_find_p(t_cub3D *data);
+void	ft_init_p(t_cub3D *data, char c);
+void	ft_free_struct(t_cub3D *data);
 
 #endif
--- Piscine/S01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v "#" | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | awk  "NR >= $FT_LINE1 && NR <= $FT_LINE2" | awk '{printf "%s%s",(NR>1?", ":""),$NF;} END{print"."}' | tr -d '\n'

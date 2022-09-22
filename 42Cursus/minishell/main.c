37ee0fb120c57eac898f738fa91c251005d13702
mabasset <matte.bassetto01@gmail.com>
2022-09-22 18:04:24 +0200

    ciao

--- 42Cursus/minishell/main.c
+++ 42Cursus/minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: mabasset <mabasset@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 15:27:15 by dforte            #+#    #+#             */
-/*   Updated: 2022/09/22 18:00:37 by mabasset         ###   ########.fr       */
+/*   Updated: 2022/09/22 18:04:19 by mabasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int main(int ac, char **av)
 	{
 		line = readline("Minishell>? ");
 		ftParser(line);
-		free(line);a
+		free(line);
+		ciao;
 	}
 }

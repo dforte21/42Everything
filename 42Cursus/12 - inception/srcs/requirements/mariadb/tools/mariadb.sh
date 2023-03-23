fd3912d3966a84df6a9f04d6b5c907e6d42ff789
dforte21 <davideforte21@outlook.it>
2023-03-23 18:25:52 +0100

    inception 125

--- 42Cursus/12 - inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ 42Cursus/12 - inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -32,10 +32,12 @@ echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH
 
 #Create database and user in the database for wordpress
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Import database in the mysql command line
 mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 fi
 
